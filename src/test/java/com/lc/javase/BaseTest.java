6aeb3be2e0e6e8389100610c8ad257ab01fefc58
gujixian <gujixian@daojia.com>
2018-05-30 15:27:01 +0800

    Add Date Utils!

--- src/test/java/com/lc/javase/BaseTest.java
+++ src/test/java/com/lc/javase/BaseTest.java
@@ -1,5 +1,6 @@
 package com.lc.javase;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
 import java.time.Instant;
@@ -8,6 +9,7 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.LinkedList;
 
 public class BaseTest {
@@ -51,5 +53,25 @@ public class BaseTest {
         System.out.println(handlers.peekFirst());
         System.out.println(handlers.peekFirst());
         System.out.println(handlers.peekFirst());
+    }
+
+    @Test
+    public void testDivideAndMod() {
+        int serviceMonth = 201802;
+        int year = serviceMonth / 100;
+        int month = serviceMonth % 100;
+        System.out.println("year:" + year + ",month:" + month);
+        for (month = 1; month <= 12; month ++) {
+            Date startCreateTime = new Date();
+            //注意设置的先后顺序:因为用的是new Date();
+            startCreateTime = DateUtils.setSeconds(startCreateTime, 0);
+            startCreateTime = DateUtils.setMinutes(startCreateTime, 0);
+            startCreateTime = DateUtils.setHours(startCreateTime, 0);
+            startCreateTime = DateUtils.setDays(startCreateTime, 1);
+            startCreateTime = DateUtils.setMonths(startCreateTime, month - 1);
+            startCreateTime = DateUtils.setYears(startCreateTime, year);
+            Date endCreateTime = DateUtils.addMonths(startCreateTime, 1);
+            System.out.println("startCreateTime:" + startCreateTime + ",endCreateTime:" + endCreateTime);
+        }
     }
 }
