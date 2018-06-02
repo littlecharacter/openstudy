af3c4f162cb54484fb622e2f16fd329d0046d287
gujixian <gujixian@daojia.com>
2018-06-02 20:29:37 +0800

    Add Encode Decode!

--- src/test/java/com/lc/javase/BaseTest.java
+++ src/test/java/com/lc/javase/BaseTest.java
@@ -3,12 +3,14 @@ package com.lc.javase;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Date;
 import java.util.LinkedList;
 
@@ -73,5 +75,15 @@ public class BaseTest {
             Date endCreateTime = DateUtils.addMonths(startCreateTime, 1);
             System.out.println("startCreateTime:" + startCreateTime + ",endCreateTime:" + endCreateTime);
         }
+    }
+
+    @Test
+    public void testEncodeAndDecode() throws UnsupportedEncodingException {
+        String source = "[{\"I\":10112,\"V\":\"1000\"},{\"I\":10113,\"V\":\"2000\"},{\"I\":10115,\"V\":\"165\"},{\"I\":10117,\"V\":\"200\"},{\"I\":10118,\"V\":\"13626\"},{\"I\":10119,\"V\":\"6600\"},{\"I\":10123,\"V\":\"9\"},{\"I\":10124,\"V\":\"16000\"},{\"I\":10125,\"V\":\"17000\"},{\"I\":20001,\"V\":\"1\"},{\"I\":20002,\"V\":\"1\"},{\"I\":20003,\"V\":\"2\"},{\"I\":20004,\"V\":\"2\"},{\"I\":20005,\"V\":\"500\"},{\"I\":20007,\"V\":\"4\"},{\"I\":20008,\"V\":\"123456789\"},{\"I\":20009,\"V\":\"1111111111111\"},{\"I\":20010,\"V\":\"987654321\"},{\"I\":20011,\"V\":\"33000\"},{\"I\":10101,\"V\":\"8000\"},{\"I\":10102,\"V\":\"3000\"},{\"I\":10103,\"V\":\"2000\"},{\"I\":10104,\"V\":\"3000\"},{\"I\":10105,\"V\":\"1000\"},{\"I\":10106,\"V\":\"1000\"},{\"I\":10107,\"V\":\"2000\"},{\"I\":10108,\"V\":\"3000\"},{\"I\":10109,\"V\":\"1000\"},{\"I\":10110,\"V\":\"1000\"},{\"I\":10111,\"V\":\"1000\"}]";
+        System.out.println("source:" + source);
+        String encode = Base64.getEncoder().encodeToString(source.getBytes("utf-8"));
+        System.out.println("encode:" + encode);
+        String decode = new String(Base64.getDecoder().decode(encode), "utf-8");
+        System.out.println("decode:" + decode);
     }
 }
