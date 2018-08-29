9b8dbc98d4738485e4fcab277632c3634b229571
gujixian <gujixian@daojia.com>
2018-08-29 17:03:05 +0800

    Modify Thread Interrupt!

--- src/test/java/com/lc/javase/thread/interrupt/InterruptStudyTest.java
+++ src/test/java/com/lc/javase/thread/interrupt/InterruptStudyTest.java
@@ -22,4 +22,35 @@ public class InterruptStudyTest {
         t.interrupt();
         TimeUnit.SECONDS.sleep(30);
     }
+
+    @Test
+    public void interruptTest() throws Exception {
+        Thread t = new Thread(() -> {
+            System.out.println("线程一启动");
+            while (!Thread.interrupted()) {
+                System.out.println("线程一执行");
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("线程一中断?" +Thread.currentThread().isInterrupted()+ ".");
+                    // 设置线程的中断转态为true，控制跳出while循环
+                    Thread.currentThread().interrupt();
+                    System.out.println("线程一中断?" +Thread.currentThread().isInterrupted()+ ".");
+                }
+            }
+            System.out.println("线程一结束");
+        });
+        t.start();
+        try {
+            TimeUnit.MILLISECONDS.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t.interrupt();
+        try {
+            TimeUnit.MILLISECONDS.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
