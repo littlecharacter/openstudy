ff4ce8550f252f44e8a2ccbc2d5d99754ba79c3c
gujixian <gujixian@daojia.com>
2019-04-29 20:54:12 +0800

    Modify Datetime!

--- src/test/java/com/lc/javase/base/DateTimeStudyTest.java
+++ src/test/java/com/lc/javase/base/DateTimeStudyTest.java
@@ -2,9 +2,26 @@ package com.lc.javase.base;
 
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class DateTimeStudyTest {
     @Test
     public void testShowDateTime() throws Exception {
         new DateTimeStudy().showDateTime();
+    }
+
+    @Test
+    public void testGetNonworkDay() throws ParseException {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
+        while (calendar.getTime().before(df.parse("2020-01-01"))) {
+            if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                System.out.println(df.format(calendar.getTime()));
+            }
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
     }
 }
