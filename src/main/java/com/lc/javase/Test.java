71d1ce85cd6f6e0222b9d119bea924a94cc286c0
gujixian <littlecharacter@qq.com>
2022-09-17 18:08:57 +0800

    更新jvm

--- src/main/java/com/lc/javase/Test.java
+++ src/main/java/com/lc/javase/Test.java
@@ -3,57 +3,34 @@ package com.lc.javase;
 import com.lc.javase.base.StringStudy;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
-    // 常量
-    public static final int SFIX = 127; // -128 int和long一样，关键是整数
-    public static final int SFIY = 128; // -129
-    public static final Integer SFI1 = 127;
-    public static final Integer SFI2 = 128;
-    public static final String SFS1 = "hello";
-    public static final String SFS2 = new String("hello");
-    public static final Date DATE1 = new Date();
-    // 类变量
-    public static int six = 127;
-    public static int siy = 128;
-    public static Integer si1 = 127;
-    public static Integer si2 = 128;
-    public static String ss1 = "hello";
-    public static String ss2 = new String("hello");
-    public static Date date2 = new Date();
-    // 实例变量
-    private int ix = 127;
-    private int iy = 128;
-    private Integer i1 = 127;
-    private Integer i2 = 128;
-    private String s1 = "hello";
-    private String s2 = new String("hello");
-    private Date date3 = new Date();
+    public static AtomicInteger num = new AtomicInteger(0);
 
-    public void test() {
-        // 局部变量
-        int mix = 127;
-        int miy = 128;
-        Integer mi1 = 127;
-        Integer mi2 = 128;
-        String ms1 = "hello";
-        String ms2 = new String("hello");
-        final String msx = "hw";
-        // 基本类型只比较值，包括引用类型和基本类型比较
-        System.out.println(Test.SFIY == Test.siy && Test.siy == this.iy && this.iy == miy);
-        // 引用类型比较地址
-        System.out.println(Test.SFI1 == Test.si1 && Test.si1 == this.i1 && this.i1 == mi1);
-        System.out.println(Test.SFI2 == Test.si2 || Test.si2 == this.i2 || this.i2 == mi2);
-        System.out.println(Test.SFS1 == Test.ss1 && Test.ss1 == this.s1 && this.s1 == ms1);
-        System.out.println(Test.SFS1 == Test.SFS2 || Test.SFS2 == Test.ss2 || Test.ss2 == this.s2 || this.s2 == ms2);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(Test.DATE1);
-        System.out.println(Test.date2);
-        Test t = new Test();
-        System.out.println(t.date3);
-        t.test();
-        while (true) {}
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("go...");
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000L);
+                    System.out.println("num = " + num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        Runnable runnable = () -> {
+            while (true) {
+                for (int i = 0; i < 500000000; i++) {
+                    num.getAndAdd(1);
+                }
+                System.out.println(Thread.currentThread().getName() + "执行结束!");
+            }
+        };
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        t1.start();
+        t2.start();
     }
 }
---
+++ src/main/java/com/lc/javase/jvm/SafePointStudy.java
@@ -0,0 +1,52 @@
+package com.lc.javase.jvm;
+
+import com.mysql.cj.jdbc.util.TimeUtil;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 安全点学习：https://zhuanlan.zhihu.com/p/286110609
+ */
+public class SafePointStudy {
+    private static final Thread t1 = new Thread(() -> {
+        while (true) {
+            for (int i = 1; i <= 1000000000; i++) {
+                boolean b = 1.0 / i == 0;
+            }
+        }
+    });
+
+    private static final Thread t2 = new Thread(() -> {
+        while (true) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(5L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            byte[] bytes = new byte[50 * 1024];
+        }
+    });
+
+    public static void main(String[] args) throws InterruptedException {
+        // new Thread(() -> {
+        //     while (true) {
+        //         long start = System.currentTimeMillis();
+        //         try {
+        //             TimeUnit.MILLISECONDS.sleep(1000L);
+        //         } catch (InterruptedException e) {
+        //             e.printStackTrace();
+        //         }
+        //         long cost = System.currentTimeMillis() - start;
+        //         (cost > 1100L ? System.err : System.out).printf("thread: %s, costs %d ms\n", Thread.currentThread().getName(), cost);
+        //     }
+        // }).start();
+        t1.start();
+        t2.start();
+        while (true) {
+            long start = System.currentTimeMillis();
+            TimeUnit.MILLISECONDS.sleep(1000L);
+            long cost = System.currentTimeMillis() - start;
+            (cost > 1100L ? System.err : System.out).printf("thread: %s, costs %d ms\n", Thread.currentThread().getName(), cost);
+        }
+    }
+}
