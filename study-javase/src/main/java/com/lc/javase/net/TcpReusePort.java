d5f56ae4f6ca6c69216b07a202ae7e5dc196d5e9
gujixian <littlecharacter@qq.com>
2023-04-10 21:04:10 +0800

    java net

---
+++ study-javase/src/main/java/com/lc/javase/net/TcpReusePort.java
@@ -0,0 +1,34 @@
+package com.lc.javase.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Arrays;
+
+/**
+ * 前提条件：启动服务端 -> nc -l 192.168.1.101 1080 和 nc -l 192.168.1.101 1080
+ *
+ * @author gujixian
+ * @since 2023/4/10
+ */
+public class TcpReusePort {
+    public static void main(String[] args) throws IOException {
+        Socket client = new Socket();
+        client.setReuseAddress(true);
+        client.bind(new InetSocketAddress(1234));
+        // client.connect(new InetSocketAddress("192.168.1.101", 1080));
+        client.connect(new InetSocketAddress("192.168.1.101", 1090));
+        InputStream in = client.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        char[] buffer = new char[1024];
+        while (true) {
+            int resultFlag = reader.read(buffer);
+            if (resultFlag != -1) {
+                System.out.println(Arrays.toString(buffer));
+            }
+        }
+    }
+}
