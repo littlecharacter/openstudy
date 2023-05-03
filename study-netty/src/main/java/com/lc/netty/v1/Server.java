012f64c3277737cea07700442552b11170a4d20f
gujixian <littlecharacter@qq.com>
2023-05-04 01:42:38 +0800

    Netty

--- study-netty/src/main/java/com/lc/netty/v1/Server.java
+++ study-netty/src/main/java/com/lc/netty/v1/Server.java
@@ -1,25 +1,13 @@
 package com.lc.netty.v1;
 
-import com.alibaba.fastjson.JSON;
-import com.lc.netty.Request;
-import com.lc.netty.Response;
-import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
-import io.netty.handler.codec.serialization.ClassResolvers;
-import io.netty.handler.codec.serialization.ObjectDecoder;
-import io.netty.handler.codec.serialization.ObjectEncoder;
 import io.netty.util.CharsetUtil;
-import io.netty.util.concurrent.EventExecutorGroup;
 
 import java.net.InetSocketAddress;
-import java.util.concurrent.TimeUnit;
 
 public class Server {
     public static void main(String[] args) throws Exception {
