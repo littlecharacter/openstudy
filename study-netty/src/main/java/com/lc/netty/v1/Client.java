1cc6544d9e0d1c696cac8329d00fe7e22d49da27
gujixian <littlecharacter@qq.com>
2023-05-04 01:44:29 +0800

    Netty

--- study-netty/src/main/java/com/lc/netty/v1/Client.java
+++ study-netty/src/main/java/com/lc/netty/v1/Client.java
@@ -1,25 +1,13 @@
 package com.lc.netty.v1;
 
-import com.alibaba.fastjson.JSON;
-import com.lc.netty.Request;
-import com.lc.netty.Response;
-import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
-import io.netty.handler.codec.serialization.ClassResolvers;
-import io.netty.handler.codec.serialization.ObjectDecoder;
-import io.netty.handler.codec.serialization.ObjectEncoder;
 import io.netty.util.CharsetUtil;
-import io.netty.util.ReferenceCountUtil;
 
 import java.net.InetSocketAddress;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
