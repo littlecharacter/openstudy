d8ae35b4f9503e2d996a7f102607ca58c004f31b
gujixian <littlecharacter@qq.com>
2023-05-10 04:39:49 +0800

    添加 study-dubbo 模块

--- study-dubbo/study-dubbo-consumer/src/main/java/com/lc/dubbo/consumer/ConsumerApplication.java
+++ study-dubbo/study-dubbo-consumer/src/main/java/com/lc/dubbo/consumer/ConsumerApplication.java
@@ -14,6 +14,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
-        ExtensionLoader extensionLoader;
     }
 }
