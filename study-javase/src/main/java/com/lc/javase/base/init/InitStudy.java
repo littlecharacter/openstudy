e580790c96fb8d7aae8f310dc1bb38c943f95ced
gujixian <littlecharacter@qq.com>
2023-05-11 18:37:37 +0800

    初始化

---
+++ study-javase/src/main/java/com/lc/javase/base/init/InitStudy.java
@@ -0,0 +1,25 @@
+package com.lc.javase.base.init;
+
+/**
+ * @author gujixian
+ * @since 2023/5/11
+ */
+public class InitStudy {
+    public InitStudy() {
+        System.out.println("Constructor is called");
+    }
+
+    {
+        System.out.println("Constructor block is called");
+    }
+
+    static {
+        System.out.println("Static block is called");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method is called");
+        new InitStudy();
+
+    }
+}
