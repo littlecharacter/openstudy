b3af424d91a36b7df0929a962db5f61e966da891
gujixian <littlecharacter@qq.com>
2023-06-29 22:30:29 +0800

    DC3

--- study-algorithm/src/main/java/com/lc/TEST.java
+++ study-algorithm/src/main/java/com/lc/TEST.java
@@ -10,72 +10,34 @@ import java.util.*;
  * @since 2023/6/13
  */
 public class TEST {
-    static class Suffix implements Comparable<Suffix> {
-        int index;
-        int rank;
-        int next;
-        public Suffix(int index, int rank, int next) {
-            this.index = index;
-            this.rank = rank;
-            this.next = next;
-        }
-        @Override
-        public int compareTo(Suffix other) {
-            if (this.rank != other.rank) {
-                return Integer.compare(this.rank, other.rank);
-            }
-            return Integer.compare(this.next, other.next);
-        }
-    }
-    static int[] buildSuffixArray(int[] arr) {
-        int n = arr.length;
-        Suffix[] suffixes = new Suffix[n];
-        // 创建后缀数组
-        for (int i = 0; i < n; i++) {
-            int rank = arr[i];
-            int next = (i + 1 < n) ? arr[i + 1] : -1;
-            suffixes[i] = new Suffix(i, rank, next);
-        }
-        // 将后缀数组按照字典序排序
-        Arrays.sort(suffixes);
-        int[] indexes = new int[n];
-        for (int k = 4; k < 2 * n; k *= 2) {
-            int rank = 0;
-            int prevRank = suffixes[0].rank;
-            suffixes[0].rank = rank;
-            indexes[suffixes[0].index] = 0;
-            // 更新排序后的后缀数组的rank值
-            for (int i = 1; i < n; i++) {
-                if (suffixes[i].rank == prevRank && suffixes[i].next == suffixes[i - 1].next) {
-                    prevRank = suffixes[i].rank;
-                    suffixes[i].rank = rank;
-                } else {
-                    prevRank = suffixes[i].rank;
-                    suffixes[i].rank = ++rank;
-                }
-                indexes[suffixes[i].index] = i;
-            }
-            // 更新下一个排序所需的next值
-            for (int i = 0; i < n; i++) {
-                int nextIndex = suffixes[i].index + k / 2;
-                suffixes[i].next = (nextIndex < n) ? suffixes[indexes[nextIndex]].rank : -1;
-            }
-            // 再次按照字典序排序
-            Arrays.sort(suffixes);
-        }
-        // 构建最终的后缀数组
-        int[] suffixArray = new int[n];
-        for (int i = 0; i < n; i++) {
-            suffixArray[i] = suffixes[i].index;
-        }
-        return suffixArray;
-    }
     public static void main(String[] args) {
-        int[] arr = {1,0,13,0,13,0};
-        int[] suffixArray = buildSuffixArray(arr);
-        System.out.println("后缀数组:");
-        for (int i = 0; i < suffixArray.length; i++) {
-            System.out.print(suffixArray[i] + " ");
+        System.out.println(new TEST().numTilings(60));
+    }
+
+    public int numTilings(int n) {
+        if (n < 1) {
+            return 0;
         }
+        long result = process(3, 0, n);
+        return (int)(result % 1000000007L);
+    }
+    // status：前一列的状态
+    // index：当前列
+    private long process(int status, int index, int n) {
+        if (index == n) {
+            return status == 3 ? 1L : 0L;
+        }
+        if (index > n) {
+            return 0L;
+        }
+        long ways = 0L;
+        if (status == 3) {
+            ways += process(3, index + 1, n) + process(3, index + 2, n) + process(2, index + 2, n) + process(1, index + 2, n);
+        } else if (status == 2) {
+            ways += process(3, index + 1, n) + process(1, index + 1, n);
+        } else if (status == 1) {
+            ways += process(3, index + 1, n) + process(2, index + 1, n);
+        }
+        return ways;
     }
 }
