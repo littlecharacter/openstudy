c25424e7ce3dcf12e0f7e5bf8f1487fe17f5cc72
gujixian <littlecharacter@qq.com>
2023-07-19 19:04:01 +0800

    study framework

---
+++ study-redis/src/main/java/com/lc/redis/TEST.java
@@ -0,0 +1,8 @@
+package com.lc.redis;
+
+/**
+ * @author gujixian
+ * @since 2023/7/19
+ */
+public class TEST {
+}
---
+++ study-test/src/main/java/com/lc/test/TEST.java
@@ -0,0 +1,8 @@
+package com.lc.test;
+
+/**
+ * @author gujixian
+ * @since 2023/7/19
+ */
+public class TEST {
+}
